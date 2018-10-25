e7a67cbeadde7f60ff98a31006dc7bc60cee3b9b
Jake Overall <joverall22@gmail.com>
2018-10-24 19:45:45 -0600

    Burgers done student handoff

---
+++ Source/Burgershack.Data/FakeDb.cs
@@ -0,0 +1,32 @@
+﻿using Burgershack.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Burgershack.Data
+{
+    public static class FakeDb
+    {
+        public static int NextId = 4;
+        public static List<Burger> Burgers = new List<Burger>() {
+            new Burger(){
+                Id = 1,
+                Name = "Plain Jane",
+                Price =  7.99m,
+                Description = "Delicious yet lacking somehow"
+            },
+            new Burger(){
+                Id = 2,
+                Name = "Plain Jane con Queso",
+                Price =  8.99m,
+                Description = "Delicioso"
+            },
+            new Burger(){
+                Id = 3,
+                Name = "Hawaiian",
+                Price =  12.99m,
+                Description = "Pineapple YUM!!!"
+            },
+        };
+    }
+}
